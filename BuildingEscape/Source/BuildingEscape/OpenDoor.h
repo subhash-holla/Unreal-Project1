b54fe6df17bf8d996c22467a4b009c78ecaf2c1d
Subhash Holla H S <subhashholla23@gmail.com>
2018-06-27 20:30:32 -0400

    Added trigger volume

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -12,18 +13,22 @@ class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UOpenDoor();
+	public:	
+		// Sets default values for this component's properties
+		UOpenDoor();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+	protected:
+		// Called when the game starts
+		virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	public:	
+		// Called every frame
+		virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
+	private:
+		UPROPERTY(VisibleAnywhere)
+		float OpenAngle = 90.0f;
+
+		UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
 };
